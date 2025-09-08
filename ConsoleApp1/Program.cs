f8b72ddb6fdd39bb72ab1e813f10cd1fe60e38fd
samyu-2 <samyuktha.ei22@bitsathy.ac.in>
2025-09-08 13:24:59 +0530

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PasswordUtility;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            passwordHelper helper = new passwordHelper();
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            var issue = helper.getPasswordIssue(password);
+
+            if (issue.Count == 0)
+            {
+                Console.WriteLine("Password is correct");
+            }
+            else
+            {
+                Console.WriteLine("Weak Password. Password must ");
+                foreach (var issues in issue)
+                {
+                    Console.WriteLine(issues);
+                }
+            }
+
+            Console.WriteLine("Masked Password: " + helper.MaskPassword(password));
+            Console.ReadLine();
+        }
+    }
+}
